064383769f60662446c92cf5802d6739921e8f84
RGiskard <rcondori@gmail.com>
2019-02-20 23:43:29 -0500

    Envio programa

--- Edgar/hello.f90
+++ Edgar/hello.f90
@@ -1,3 +1,13 @@
-Program Hello
-      Print *, "Hello World"
-End Program Hello
+function func(i) result(j)
+    integer, intent(in) :: i ! input
+    integer             :: j ! output
+    j = i**2 + i**3
+end function func
+
+program xfunc
+    implicit none
+    integer :: i
+    integer :: func
+    i = 3
+    print*,"sum of the square and cube of",i," is",func(i)
+ end program xfunc
